2d7dc026ec4c1e37557eb4099396e62d307aebfb
Springhawk27 <sajjad0mahmud@gmail.com>
2022-10-12 03:39:50 +0600

    minor changes and readme.md file edited

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -12,9 +12,9 @@ const Quiz = () => {
         <div>
             <section className="text-gray-600 body-font">
                 <div className="container px-5  mx-auto">
-                    <div className="lg:w-2/3 mx-auto">
+                    <div className="mx-auto">
                         <div className="flex flex-wrap w-full  py-32 px-10 relative mb-4">
-                            <img alt="gallery" className="w-full object-contain h-full object-center block opacity-25 absolute inset-0" src={binary} />
+                            <img alt="gallery" className="w-full object-cover h-full object-center block opacity-25 absolute inset-0" src={binary} />
                             <div className="text-center relative z-10 w-full">
                                 <h2 className="text-2xl text-gray-900 font-medium title-font mb-2">Ace the Interview</h2>
                                 <p className="leading-relaxed">Take these interactive quizzes now and crack the interview</p>
