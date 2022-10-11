c3b6bf1a284ad0483cca6488907271da0e66a888
Springhawk27 <sajjad0mahmud@gmail.com>
2022-10-11 18:26:16 +0600

    all the necessary packages installed

--- src/App.js
+++ src/App.js
@@ -4,6 +4,9 @@ function App() {
   return (
     <div className="App">
       <h1>QuizTime</h1>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
     </div>
   );
 }
