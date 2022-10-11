ffabbaa209feccd26c216b4a8f0c7dd758e91c82
Springhawk27 <sajjad0mahmud@gmail.com>
2022-10-11 19:20:54 +0600

    router setup and navbar implemented

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import './App.css';
+import Blog from './components/Blog/Blog';
+import Quiz from './components/Quiz/Quiz';
+import Statistics from './components/Statistics/Statistics';
+import Main from './layouts/Main';
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          // loader: () => fetch('tshirts.json'), 
+          element: <Quiz></Quiz>
+        },
+        {
+          path: '/statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        }
+      ]
+    }
+  ])
+
   return (
-    <div className="App">
-      <h1>QuizTime</h1>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+    <div >
+      <RouterProvider router={router}></RouterProvider>
+
     </div>
   );
 }
---
+++ src/components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Header = () => {
+    return (
+        <header className="text-gray-600 body-font bg-purple-400" >
+            <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+                <Link to='/' className="flex title-font font-medium items-center text-sky-200 mb-4 md:mb-0">
+
+                    <span className="ml-3 text-2xl font-bold">QuizTime</span>
+                </Link>
+                <nav className="md:ml-auto flex flex-wrap items-center text-gray-50 font-bold justify-center">
+
+                    <Link to='/' className="mr-5 hover:text-gray-900">Quiz</Link>
+                    <Link to='/statistics' className="mr-5 hover:text-gray-900">Statistics</Link>
+                    <Link to='/blog' className="mr-5 hover:text-gray-900">Blog</Link>
+                </nav>
+
+            </div>
+        </header>
+    );
+};
+
+export default Header;
