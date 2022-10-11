b57ac46398ddfff791b68310feeafe27bf28c554
Springhawk27 <sajjad0mahmud@gmail.com>
2022-10-12 03:12:12 +0600

    header and question answer with toast implemented

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import QuizCard from '../QuizCard/QuizCard';
 
+import binary from '../../images/binary.jpg'
+
 const Quiz = () => {
     const quizes = useLoaderData();
     // console.log(quizes.data)
@@ -9,7 +11,18 @@ const Quiz = () => {
     return (
         <div>
             <section className="text-gray-600 body-font">
-                <div className="container px-5 py-24 mx-auto">
+                <div className="container px-5  mx-auto">
+                    <div className="lg:w-2/3 mx-auto">
+                        <div className="flex flex-wrap w-full  py-32 px-10 relative mb-4">
+                            <img alt="gallery" className="w-full object-contain h-full object-center block opacity-25 absolute inset-0" src={binary} />
+                            <div className="text-center relative z-10 w-full">
+                                <h2 className="text-2xl text-gray-900 font-medium title-font mb-2">Ace the Interview</h2>
+                                <p className="leading-relaxed">Take these interactive quiz now and crack the interview</p>
+
+                            </div>
+                        </div>
+
+                    </div>
                     <div className="flex flex-wrap -m-4">
 
                         {
@@ -22,8 +35,8 @@ const Quiz = () => {
 
                     </div>
                 </div>
-            </section>
-        </div>
+            </section >
+        </div >
     );
 };
 
--- src/components/QuizQuestions/QuizQuestions.js
+++ src/components/QuizQuestions/QuizQuestions.js
@@ -1,39 +1,99 @@
 import React from 'react';
+import toast, { Toaster } from 'react-hot-toast';
+import { EyeIcon } from '@heroicons/react/24/solid'
+
+
+
+
+
 
 const QuizQuestions = ({ questions }) => {
-    console.log(questions)
-    const qs = questions.map(question => {
-        console.log(question)
-    })
+    // console.log(questions)
+
     return (
         <>
             {
                 questions.map(question => {
+                    console.log(question)
+                    // console.log(question.correctAnswer)
+                    const correctAns = question.correctAnswer
+
+                    const notifyCorrect = (correctAns) => toast.success(`Correct answer is - ${correctAns}`, {
+                        style: {
+                            border: '1px solid green',
+                            padding: '16px',
+                            color: '#713200',
+                        },
+                        iconTheme: {
+                            primary: 'green',
+                            secondary: '#FFFAEE',
+                        },
+                    });
                     return (
                         <div className="lg:w-2/3 w-full mx-auto overflow-auto">
                             <div className="w-full">
-                                <div className="bg-blue-100 rounded flex p-4 h-full items-center">
-                                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="text-purple-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
-                                        <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                                        <path d="M22 4L12 14.01l-3-3"></path>
-                                    </svg>
-                                    <span className="title-font font-medium"> {question.question.slice(3, -4)}</span>
-                                </div>
-                            </div>
+                                <div className="bg-blue-100 rounded flex p-4 h-full items-center justify-between">
 
+                                    <span className="title-font font-medium">
+                                        {question.question.slice(3, -4)}</span>
+                                    <EyeIcon onClick={() => notifyCorrect(correctAns)} className="ml-2 h-6 w-6 text-blue-400"></EyeIcon>
+                                </div>
+
+                            </div>
                             <div className="flex flex-wrap lg:w-full sm:mx-auto sm:mb-2 -mx-2">
-                                <div className="py-2 sm:w-1/2 w-full">
-                                    <div className="bg-purple-100 rounded flex p-4 h-full items-center">
-                                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="3" className="text-purple-500 w-6 h-6 flex-shrink-0 mr-4" viewBox="0 0 24 24">
-                                            <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
-                                            <path d="M22 4L12 14.01l-3-3"></path>
-                                        </svg>
-                                        <span className="title-font font-medium">Authentic Cliche Forage</span>
-                                    </div>
-                                </div>
+                                {
+                                    question.options.map(option => {
+                                        const notify = () => toast.success(`Your answer is correct`, {
+                                            style: {
+                                                border: '1px solid green',
+                                                padding: '16px',
+                                                color: '#713200',
+                                            },
+                                            iconTheme: {
+                                                primary: 'green',
+                                                secondary: '#FFFAEE',
+                                            },
+                                        });
+                                        const notify2 = () => toast.error(`Your answer is  wrong`, {
+                                            style: {
+                                                border: '1px solid red',
+                                                padding: '16px',
+                                                color: '#713200',
+                                            },
+                                            iconTheme: {
+                                                primary: 'red',
+                                                secondary: '#FFFAEE',
+                                            },
+                                        });
+                                        const displayToast = (correctAns, option) => {
+                                            if (correctAns === option) {
+                                                notify()
 
+                                            }
+                                            else {
+                                                notify2()
+                                            }
+                                        }
 
+                                        return (
+
+                                            <div className="p-2 sm:w-1/2 w-full ">
+                                                <div onClick={() => displayToast(correctAns, option)} className="bg-purple-100 rounded flex p-4 h-full items-center cursor-pointer">
+
+                                                    <div >
+                                                        <span className="title-font font-medium">{option}</span>
+
+                                                        <Toaster position="top-center"
+                                                            reverseOrder={false} />
+                                                    </div>
+
+                                                </div>
+                                            </div>
+                                        )
+                                    })
+                                }
                             </div>
+
                         </div>
                     )
                 })
