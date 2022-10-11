96c2aa7538d9435ae2bc72b95524adb4d6c21e2d
Springhawk27 <sajjad0mahmud@gmail.com>
2022-10-12 04:37:52 +0600

    minor changes

--- src/components/TakeQuiz/TakeQuiz.js
+++ src/components/TakeQuiz/TakeQuiz.js
@@ -13,7 +13,7 @@ const TakeQuiz = () => {
         <section className="text-gray-600 body-font">
             <div className="container px-5 py-24 mx-auto">
                 <div className="flex flex-col text-center w-full mb-20 justify-center items-center">
-                    <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded" alt="hero" src={logo} />
+                    <img className="lg:w-2/6 md:w-3/6 w-5/6 mb-10 object-cover object-center rounded bg-purple-200" alt="hero" src={logo} />
 
                     <h1 className="sm:text-4xl text-3xl font-medium title-font mb-2 text-gray-900">Quiz on {name}</h1>
                     <p className="lg:w-2/3 mx-auto leading-relaxed text-base">Mastery {name} by answering this {total} questions</p>
