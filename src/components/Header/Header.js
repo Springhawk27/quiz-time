9986f92b737637464a2028028b9b7ba90e0cbbc6
Springhawk27 <sajjad0mahmud@gmail.com>
2022-10-12 15:47:27 +0600

    sticky class removed

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     return (
-        <header className="text-gray-600 body-font bg-purple-400 sticky top-0" >
+        <header className="text-gray-600 body-font bg-purple-400" >
             <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
                 <Link to='/' className="flex title-font font-medium items-center text-sky-300 mb-4 md:mb-0">
 
